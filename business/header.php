a18c26c53a0e4405c5222253ce1dfa6f1f12cecb
Preetu098 <preetu098@gmail.com>
2022-11-18 17:51:01 +0530

    add changes

--- business/header.php
+++ business/header.php
@@ -137,7 +137,7 @@ define('SITE_URL', getSiteURL());
                                <a href="https://webmail.raichandgroup.com/"><button type="button" class="btn btn-dark" data-toggle="tooltip" data-placement="top" title="Personnel Login"> <i class="fa fa-envelope" aria-hidden="true"></i></button></a>
                             </li>&nbsp;
                              <li class="nav-item">
-                               <a href="<?php echo SITE_URL;?>login.php"><button type="button" class="btn btn-dark" > <i class="fa fa-sign-in" aria-hidden="true"></i></button></a>
+                               <a href="<?php echo SITE_URL;?>login.php" target="_blank"><button type="button" class="btn btn-dark" > <i class="fa fa-sign-in" aria-hidden="true"></i></button></a>
                             </li>
                         </ul>
                     </div>
