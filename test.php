61b4f5a3b6412ccb6da9b4a1620d6090aa1b3a89
rohitj <rjohri22@gmail.com>
2022-11-26 18:07:29 +0530

    have added

--- test.php
+++ test.php
@@ -1,19 +1,8 @@
 <?php
-$plaintext_password=password_hash("12345",  PASSWORD_DEFAULT);
- 
- 
+  $hash = password_hash("12345", 
+  PASSWORD_DEFAULT);
+
+  echo $hash;
+  
 // echo $hash; exit;
-
-$verify = password_verify($plaintext_password,`$2y$10$bJWQ5etL8hjbq7YvWTqY3.U0v5/It9dt7u2lHeMNNFKKTXkJHifTu`);
-
-
-if($verify ==true){
-    echo "Password match";
-}
-else{
-    echo "Password mismatch";
-}
-
-
-
 ?>
