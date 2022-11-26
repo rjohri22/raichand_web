c8abb4d3b7200a8287b541e7ef6832fce2ecd59a
rohitj <rjohri22@gmail.com>
2022-11-26 16:57:08 +0530

    have added

--- dbcon.php
+++ dbcon.php
@@ -5,7 +5,7 @@ $con = "";
  
 
 if($host == 'raichandgroup.com'){
-    $con = mysqli_connect('localhost', 'raichand_website_user', 'modern@2022', 'raichand_website') or die('not connected');   
+    $con = mysqli_connect('localhost', 'raichand_website_user', '6zE65Ybr6iRLAYXX', 'raichand_website') or die('not connected');   
 }
 else{
 
